8f1f8e285764296efcf3735691031c445c57a558
unknown <yummyfunnybunny13@gmail.com>
2021-01-29 10:20:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// ANCHOR -- Document Selectors
+const countdown = document.querySelector(".countdown");
+
+const interval = setInterval(() => {
+  // get the current date and deadline date
+  const deadline = new Date(2021, 0, 30, 12, 00, 00);
+  const current = new Date();
+
+  const diff = deadline - current;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
+  const minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
+  const seconds = Math.floor((diff / 1000) % 60) + "";
+
+  countdown.innerHTML = `
+<div data-content="Days">${days.length === 1 ? `0${days}` : days}</div>
+<div data-content="Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+<div data-content="Minutes">${
+    minutes.length === 1 ? `0${minutes}` : minutes
+  }</div>
+<div data-content="Seconds">${
+    seconds.length === 1 ? `0${seconds}` : seconds
+  }</div>
+`;
+
+  console.log(`diff: ${diff}`);
+  console.log(`days: ${days}`);
+  console.log(`hours: ${hours}`);
+  console.log(`minutes: ${minutes}`);
+  console.log(`seconds: ${seconds}`);
+
+  if (diff < 0) {
+    clearInterval(interval);
+    countdown.innerHTML = `<h1>Here We Go!!!</h1>`;
+  }
+
+  document.querySelector(".reset").addEventListener("click", () => {
+    clearInterval(interval);
+
+    const divs = document.querySelectorAll(".countdown div");
+
+    divs.forEach((div) => {
+      div.innerHTML = "00";
+    });
+  });
+}, 1000);
+
+// ANCHOR -- Event Listeners --
